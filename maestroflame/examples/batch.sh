ca9e338b1b2cccb136ff24b68387ae3bd28a4e0e
doreenfan <dfan@lbl.gov>
2022-02-11 13:26:11 -0800

    Update perlmutter job script to use multiple GPUs

--- maestroflame/examples/batch.sh
+++ maestroflame/examples/batch.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH -C gpu
-#SBATCH -t 0:30:00
+#SBATCH -t 4:00:00
 #SBATCH -A m3329_g
 #SBATCH -q regular
-#SBATCH -n 1     # number of nodes
-#SBATCH --ntasks-per-node=1
-#SBATCH -c 128
+#SBATCH -n 4     # number of nodes
+#SBATCH --ntasks-per-node=4
+#SBATCH -c 32
 #SBATCH --gpus-per-task=1     # reserve 1 (of four) GPUs per task
 
 module load python pytorch/1.9.0
